bd55be8c713e47d898912f5b33b73327db1a5fe6
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-06-16 01:39:06 -0700

    Add test for ReverseList

--- list_test.go
+++ list_test.go
@@ -1,8 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"strings"
 	"testing"
 )
+
+func TestReverseList(t *testing.T) {
+	expectations := []TestExpectation{
+		{"()", "nil"},
+		{"(1)", "(1)"},
+		{"(1 2 3)", "(3 2 1)"},
+	}
+
+	for _, exp := range expectations {
+		reader := bufio.NewReader(strings.NewReader(exp.arg))
+		result := Read(reader).(*LispList)
+		actual := LispObject2String(ReverseList(result))
+		if actual != exp.expected {
+			t.Errorf("TestReverseList: Expected %v => %s, but got %s",
+				exp.arg, exp.expected, actual)
+		}
+
+	}
+}
 
 func TestNth(t *testing.T) {
 	a := NewList(1, 2, 3)
